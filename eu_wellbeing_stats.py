57c87fd3782eed18b075673965a2f776d0473387
Tatjana Kolhapure <tatjanagkolhapure@gmail.com>
2017-08-23 22:58:49 +0100

    set up database connection, collection call and a route

--- eu_wellbeing_stats.py
+++ eu_wellbeing_stats.py
@@ -1,12 +1,39 @@
 from flask import Flask
+from flask import render_template
+from pymongo import MongoClient
+import json
 
 app = Flask(__name__)
 
+MONGODB_HOST = 'localhost'
+MONGODB_PORT = 27017
+DBS_NAME = 'EuropeStats'
+COLLECTION_NAME = 'wellbeing'
 
-@app.route('/')
-def hello_world():
-    return 'Hello World!'
+
+@app.route("/")
+def index():
+    """
+    A Flask view to serve the main dashboard page.
+    """
+    return render_template("index.html")
 
 
-if __name__ == '__main__':
-    app.run()
+@app.route("/EuropeStats/wellbeing")
+def europe_wellbeing():
+    """
+    A Flask view to serve the project data from
+    MongoDB in JSON format.
+    """
+    # Open a connection to MongoDB using a with statement such that the
+    # connection will be closed as soon as we exit the with statement
+    with MongoClient(MONGODB_HOST, MONGODB_PORT) as conn:
+        # Define which collection we wish to access
+        collection = conn[DBS_NAME][COLLECTION_NAME]
+        wellbeing = collection.find()
+        # Convert data to a list in a JSON object and return the JSON data
+        return json.dumps(list(wellbeing))
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
