ebf4759b983a82c562a70c8969de971478604bad
Tatjana Kolhapure <tatjanagkolhapure@gmail.com>
2017-09-12 22:51:34 +0100

    use new mongo database created on heroku, get variables from heroku

--- eu_wellbeing_stats.py
+++ eu_wellbeing_stats.py
@@ -2,12 +2,12 @@ from flask import Flask
 from flask import render_template
 from pymongo import MongoClient
 import json
+import os
 
 app = Flask(__name__)
 
-MONGODB_HOST = 'localhost'
-MONGODB_PORT = 27017
-DBS_NAME = 'europeStats'
+MONGO_URI = os.getenv('MONGODB_URI', 'mongodb://localhost:27017')
+DBS_NAME = os.getenv('MONGO_DB_NAME', 'europeStats')
 COLLECTION_NAME = 'wellbeing'
 
 
